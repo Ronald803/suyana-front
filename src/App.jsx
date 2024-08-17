20637be2ac0f61495f2c7d3cad3292b8a8c0cb19
Ronald803 <ronaldblancobalboa@gmail.com>
2024-08-16 23:41:01 -0400

    Implement routing between new basic pages: CalendarPage, EmployeesPage, PatientsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { BrowserRouter, useRoutes } from 'react-router-dom'
 import './App.css'
+import CalendarPage from './components/pages/CalendarPage'
+import EmployeesPage from './components/pages/EmployeesPage'
 import HomePage from './components/pages/HomePage'
+import PatientsPage from './components/pages/PatientsPage'
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <HomePage /> },
+    { path: '/calendar', element: <CalendarPage /> },
+    { path: '/patients', element: <PatientsPage /> },
+    { path: '/employees', element: <EmployeesPage /> }
+  ])
+  return routes
+}
 
 function App() {
   return (
     <>
-      <div className=''>
-        <HomePage/>
+      <div>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
       </div>
     </>
   )
