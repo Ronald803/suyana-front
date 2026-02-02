55c58b86d67d0b46194374a6921f611d35736737
Ronald803 <ronaldblancobalboa@gmail.com>
2026-02-02 00:08:10 -0400

    Use useAuth and useRequireAuth hooks in EmployeesPage

--- src/components/pages/EmployeesPage.jsx
+++ src/components/pages/EmployeesPage.jsx
@@ -1,23 +1,16 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import EmployeesGrid from "../organisms/EmployeesGrid";
 import AddNewEmployeeOption from "../organisms/AddNewEmployeeOption";
+import { useRequireAuth } from "../../hooks/useRequireAuth";
+import { useAuth } from "../../hooks/useAuth";
 
 function EmployeesPage() {
-  const navigate = useNavigate();
-  const [rol, setRol] = useState("");
-  useEffect(() => {
-    const token = localStorage.getItem("t");
-    setRol(localStorage.getItem("r"));
-    if (token.length < 1) {
-      navigate("/");
-    }
-  }, []);
+  useRequireAuth();
+  const { isAdmin } = useAuth();
   return (
     <div className="flex justify-center">
       <div>
         <EmployeesGrid />
-        {rol == "administrador" && <AddNewEmployeeOption />}
+        {/* {isAdmin && <AddNewEmployeeOption />} */}
       </div>
     </div>
   );
