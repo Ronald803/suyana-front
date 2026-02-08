d01a017d140a98038bc4b26eb91782ac901dbcb7
Ronald803 <ronaldblancobalboa@gmail.com>
2026-02-08 18:49:06 -0400

    Update NAE palette colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-import colors from 'tailwindcss/colors'
+import colors from "tailwindcss/colors";
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors:{
+    colors: {
       ...colors,
-      "primary": "#FF5988",
-      "secondary": "#FF85A8",
-      "tertiary": "#FFB8CC",
-      "fourth": "#FFFFF"
-      },
-      fontFamily: { 
-        Montserrat: ["Montserrat", "sans-serif"],
-      },
+      primary: "#001855",
+      secondary: "#002172",
+      tertiary: "#012A90",
+      fourth: "#FFFFF",
+    },
+    fontFamily: {
+      Montserrat: ["Montserrat", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
